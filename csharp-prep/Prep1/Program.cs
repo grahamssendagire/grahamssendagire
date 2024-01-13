373d9dff915667b93f3fc032abc669c72a0ce1ef
graham ssendagire <ssendagiregraham745@gmail.com>
2024-01-13 20:37:26 +0300

    first assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-   Console.Write("what is your favourite color");
-   String color = Console.ReadLine();
-   Console.WriteLine(color);
-   
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
 
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
