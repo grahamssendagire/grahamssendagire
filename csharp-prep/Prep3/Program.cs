14443d6036f47a831acc14cb8d1763c6e58deb73
graham ssendagire <ssendagiregraham745@gmail.com>
2024-01-14 11:29:10 +0300

    Core Requirement 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+     
+      Console.Write("What is the magic number?");
+      string answer1 = Console.ReadLine();
+      int magicNumber = int.Parse(answer1);
+
+       Random randomGenerator = new Random();
+        int Number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+      
+      if (magicNumber < guess) 
+       {
+        Console.WriteLine("Higher");
+       }
+      else if (magicNumber > guess)
+       {
+        Console.WriteLine("Lower");
+       }
+      else
+       {
+        Console.WriteLine("You guessed it !");
+       }
+
+    }    
 }
