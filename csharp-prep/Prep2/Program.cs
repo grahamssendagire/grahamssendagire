efa8d16e3734ec47542ff086d4859390b40aef68
graham ssendagire <ssendagiregraham745@gmail.com>
2024-01-14 08:32:49 +0300

    upto requirement 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,41 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+        int x = int.Parse(gradePercentage);
+        if (x>=90)
+        {
+            Console.WriteLine("Your grade is A ");
+        }
+        else if  (x>=80)
+        {
+            Console.WriteLine("Your grade is B ");
+        }
+        else if  (x>=70)
+        {
+            Console.WriteLine(" Your grade is C ");
+        }
+          else if  (x>=60)
+        {
+            Console.WriteLine(" Your grade is: D ");
+        }
+        else 
+        {
+            Console.WriteLine(" Your grade is: F ");
+        }
+        if (x>=70)
+        {
+            Console.WriteLine("Congratulations You Excelled! ");
+        }
+        else
+        {
+            Console.WriteLine("Oooooooh Dear,Sorry Try Again Next Time");
+        }
     }
+
 }
