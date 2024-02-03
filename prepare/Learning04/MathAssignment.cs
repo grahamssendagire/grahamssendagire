79ead437b287f5557f0e2cbc809f4ffe487dc974
graham ssendagire <ssendagiregraham745@gmail.com>
2024-02-03 23:32:08 +0300

    g

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -4,12 +4,33 @@ public class MathAssignment: Assignment
 //Add the attributes as private member variables. Make sure that you do not create new member variables for the ones you inherited from the base class.
              private string _textbookSection;
              private string _problems;
- // Create a constructor for your class that accepts all four parameters, have it call the base class constructor to set the base class attributes that way. 
+ // Create a constructor for your class that accepts all four parameters, have it call the base class constructor to set the base class attributes that way.
+ // Notice the syntax here that the MathAssignment constructor has 4 parameters and then
+// it passes 2 of them directly to the "base" constructor, which is the "Assignment" class constructor. 
              public MathAssignment(string studentName,string topic,string textbookSection,string problems): base(studentName,topic)
              { 
-                _studentName = studentName;
-                _topic = topic;
+            // here we stick two only two varriables since the two have been passed dirrectly to the base class
                 _textbookSection = textbookSection;
                 _problems = problems;
              }        
-}
+            // Add the method for GetTextBookSection() to return the TextBookSection.
+             public string GetTextBookSection()
+             {
+                return _textbookSection;
+             }
+             //Add the method for GetProblems() to return the Problems.
+             public string GetProblems()
+             {
+                return _problems;
+             }
+            // Add the GetHomeworkList() method
+            public string GetHomeworkList()
+            {
+                return _textbookSection + " " + _problems;
+            }
+
+
+
+
+
+}       
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,11 +4,16 @@ class Program
 {
     static void Main(string[] args)
 
-    {    // Create a base "Assignment" object.
-        // Create a simple assignment, call the method to get the summary, and then display it to the screen.(create a base assignment object)
+    { // Create a base "Assignment" object.
+      // Create a simple assignment, call the method to get the summary, and then display it to the screen.(create a base assignment object)
         Assignment assignment1 = new Assignment("Samuel Bennett","Multiplication");
         
         Console.WriteLine(assignment1.GetSummary());
+      // creating a new MathAssignment object and set its values. Make sure to test both the GetSummary() and the GetHomeworkList() methods
+        MathAssignment assignment2 = new MathAssignment("Roberto Rodriguez","Fractions","Section 7.3","problem 8-9");
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine(assignment2.GetSummary());
     }
+
 }
  
