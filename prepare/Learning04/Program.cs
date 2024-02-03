58bb0fef6d06ce83383d570ebb9ac7a00c04c96c
graham ssendagire <ssendagiregraham745@gmail.com>
2024-02-03 22:12:20 +0300

    Good start

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -3,8 +3,12 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
+
+    {    // Create a base "Assignment" object.
+        // Create a simple assignment, call the method to get the summary, and then display it to the screen.(create a base assignment object)
+        Assignment assignment1 = new Assignment("Samuel Bennett","Multiplication");
+        
+        Console.WriteLine(assignment1.GetSummary());
     }
 }
  
