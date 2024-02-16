2e379f4b2a54b64acd05e2a061af5d000bf9382c
graham ssendagire <ssendagiregraham745@gmail.com>
2024-02-16 22:47:28 +0300

    k

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -7,22 +7,23 @@ namespace VideoComments
         static void Main(string[] args)
         {
             List<Video> videos = new List<Video>();
+            // An object on the first video.
 
             Video v1 = new Video("Introduction to SoapMaking", "Sun Bear", 500);
             v1.Comments.Add(new Comment("Alice", "Great video!"));
             v1.Comments.Add(new Comment("Bob", "Thanks for the tutorial!"));
             v1.Comments.Add(new Comment("Charlie", "Very helpful, thanks!"));
             videos.Add(v1);
-
+            // An object on the second video.
             Video v2 = new Video("SoapMaking  Concepts", "Jean Carper", 200);
             v2.Comments.Add(new Comment("David", "Nice explanation of the Concepts!"));
             v2.Comments.Add(new Comment("Eva", "Very informative, thanks!"));
             videos.Add(v2);
-
+            // An object on the third video.
             Video v3 = new Video("Mixing Tips in SoapMaking", "RoseMary GladStar", 400);
             v3.Comments.Add(new Comment("Frank", "This saved me a lot of time!"));
             videos.Add(v3);
-
+            // Loop on the videos
             foreach (Video v in videos)
             {
                 Console.WriteLine("Title: " + v.Title);
